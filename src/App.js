fe87f64aa6f0eca6204729f2e5467215d06284fa
konyaru <orira4666@gmail.com>
2021-03-28 23:54:54 +0900

    いらないコメントアウトなどを削除しました

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import React from 'react';
 import { Main } from './Main.jsx';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Present } from './Present.jsx';
-import { createStyles, makeStyles } from '@material-ui/styles';
-
-// const useStyles = makeStyles(() => 
-//   createStyles({
-//     "main": {
-//       display: "grid",
-//       alignItems: "center",
-//       justify: "center"
-//     }
-//   })
-// );
 
 function App() {
   return(
